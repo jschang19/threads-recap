20c756436409457fca5ab2829a99fefa96efbe4e
jschang19 <wwwsean13579@gmail.com>
2025-12-25 21:58:08 +0800

    chore: added compression and appId

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  appId: 'tks-recap',
   compatibilityDate: '2025-12-25',
   spaLoadingTemplate: true,
   css: ['~/assets/css/tailwind.css'],
@@ -12,4 +13,7 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  nitro: {
+    compressPublicAssets: true,
+  }
 })
